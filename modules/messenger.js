9a79c6de0cfe7ca00c5616d5fb55cdeabece2da3
Laurens Panier <ozafy@borknet.org>
2020-06-02 23:06:40 +0200

    Added reverse

--- modules/messenger.js
+++ modules/messenger.js
@@ -2,14 +2,16 @@ exports.message = function (topic, message) {
     xml = message.toString();
     var parseString = require('xml2js').parseString;
     parseString(xml, function (err, result) {
-        if(result.lc) {
-                module.exports.parseLoc(result.lc.$);
+        if (result.lc) {
+            module.exports.parseLoc(result.lc.$);
         }
     });
 }
 
 exports.parseLoc = function (loc) {
-var idParts = loc.id.split(":");
+    console.dir(loc);
+    var idParts = loc.id.split(":");
     var mPoweredUp = require('./powered-up');
-    mPoweredUp.setPower(idParts[0],idParts[1],loc.V);
+    var power = (loc.dir =='true' ? loc.V : loc.V * -1);
+    mPoweredUp.setPower(idParts[0], idParts[1], power);
 }
